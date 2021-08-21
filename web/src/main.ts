7f2060d45246ad35290af2db53b8200777a40f2a
joe052 <joekaranjasenior52@gmail.com>
2021-08-21 15:42:29 +0300

    modified main.ts

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,7 +1,22 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+/**BOOTSTRAP WITH platformBrowserDynamic()
+ * WORKS WITH APP MODULE
+ * Also the App.ts component has to be marked [standalone: false]
+ * Ensure BrowserModule is imported in app.module
+ */
+import { platformBrowser } from '@angular/platform-browser';
 import { AppModule } from './app/app.module';
 
+platformBrowser()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+/**DEFAULT CODE THAT BOOTSTRAPS WITH
+ * bootstrapApplication(AppComponent, appConfig)
+ * WORKS WITH APPCONFIG FILE
+ * Also the App.ts component has to be marked [standalone: true,]
+ */
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { appConfig } from './app/app.config';
+// import { App } from './app/app';
+
+// bootstrapApplication(App, appConfig).catch((err) => console.error(err));
