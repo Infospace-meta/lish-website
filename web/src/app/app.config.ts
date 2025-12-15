af27843e9a8a402e83ca5846d135079659fd0880
joe052 <joekaranjasenior52@gmail.com>
2025-12-15 05:11:59 +0300

    updating

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -1,21 +1,29 @@
-// import {
-//   ApplicationConfig,
-//   provideBrowserGlobalErrorListeners,
-//   provideZoneChangeDetection,
-// } from '@angular/core';
-// import { provideRouter } from '@angular/router';
-// import { routes } from './app.routes';
+import {
+  ApplicationConfig,
+//   importProvidersFrom,
+  provideBrowserGlobalErrorListeners,
+  //   provideZoneChangeDetection,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
 // import { provideStore } from '@ngrx/store';
 // import { provideEffects } from '@ngrx/effects';
 // import { todoReducer } from './state/todos/todo.reducers';
 // import { TodoEffects } from './state/todos/todo.effects';
+// import { IonicStorageModule, provideStorage } from '@ionic/storage-angular';
+// import { Drivers } from '@ionic/storage';
 
-// export const appConfig: ApplicationConfig = {
-//   providers: [
-//     provideBrowserGlobalErrorListeners(),
-//     provideZoneChangeDetection({ eventCoalescing: true }),
-//     provideRouter(routes),
-//     provideStore({ todoState: todoReducer }),
-//     provideEffects([TodoEffects]),
-//   ],
-// };
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),    
+    provideRouter(routes),
+    // provideStore({ todoState: todoReducer }),
+    // provideEffects([TodoEffects]),
+    // importProvidersFrom(
+    //   IonicStorageModule.forRoot({
+    //     name: '__mydb',
+    //     driverOrder: [Drivers.IndexedDB, Drivers.LocalStorage],
+    //   })
+    // ),
+  ],
+};
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { App } from './app';
-import { RouterModule } from '@angular/router';
-import { routes } from './app.routes';
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { App } from './app';
+// import { RouterModule } from '@angular/router';
+// import { routes } from './app.routes';
 
-@NgModule({
-  declarations: [App],
-  imports: [BrowserModule, RouterModule.forRoot(routes)],
-  providers: [provideHttpClient(withInterceptors([authTokenInterceptor]))],
-  bootstrap: [App],
-})
-export class AppModule {}
+// @NgModule({
+//   declarations: [App],
+//   imports: [BrowserModule, RouterModule.forRoot(routes)],
+//   providers: [provideHttpClient(withInterceptors([authTokenInterceptor]))],
+//   bootstrap: [App],
+// })
+// export class AppModule {}
