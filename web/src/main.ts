3276dc637d3c96745dc4c595a0ae84091f98c9ec
joe052 <joekaranjasenior52@gmail.com>
2025-12-15 04:37:44 +0300

    updating

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,20 +3,20 @@
  * Also the App.ts component has to be marked [standalone: false]
  * Ensure BrowserModule is imported in app.module
  */
-import { platformBrowser } from '@angular/platform-browser';
-import { AppModule } from './app/app.module';
+// import { platformBrowser } from '@angular/platform-browser';
+// import { AppModule } from './app/app.module';
 
-platformBrowser()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+// platformBrowser()
+//   .bootstrapModule(AppModule)
+//   .catch((err) => console.error(err));
 
 /**DEFAULT CODE THAT BOOTSTRAPS WITH
  * bootstrapApplication(AppComponent, appConfig)
  * WORKS WITH APPCONFIG FILE
  * Also the App.ts component has to be marked [standalone: true,]
  */
-// import { bootstrapApplication } from '@angular/platform-browser';
-// import { appConfig } from './app/app.config';
-// import { App } from './app/app';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { App } from './app/app';
 
-// bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
