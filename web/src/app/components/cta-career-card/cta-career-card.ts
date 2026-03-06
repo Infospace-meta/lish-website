e48132222689b0d8deb54fb34a47f7fb6fc9c780
immanuel kimani <ikimanimbugua@gmail.com>
2026-03-06 13:10:39 +0300

    message

---
+++ web/src/app/components/cta-career-card/cta-career-card.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cta-career-card',
+  imports: [],
+  template: `
+    <p>
+      cta-career-card works!
+    </p>
+  `,
+})
+export class CtaCareerCard {
+
+}
