30eca93466e01e1baedb30264fa1df92e0df62f3
immanuel kimani <ikimanimbugua@gmail.com>
2026-03-06 13:08:00 +0300

    message

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,4 +1,18 @@
 import { Routes } from '@angular/router';
+/** New Revamp Pages */
+import { HomePage } from './pages/home-page/home-page';
+import { ContactPage } from './pages/contact-page/contact-page';
+import { PartnerPage } from './pages/partner-page/partner-page';
+import { CareerPage } from './pages/career-page/career-page';
+import { AboutPage } from './pages/about-page/about-page';
+import { SuccessPage } from './pages/success-page/success-page';
+import { TechServicesPage } from './pages/tech-services-page/tech-services-page';
+import { UpskillingSevicesPage } from './pages/upskilling-sevices-page/upskilling-sevices-page';
+import { OurFacilityPage } from './pages/our-facility-page/our-facility-page';
+import { ProgramsPage } from './pages/programs-page/programs-page';
+import { BpoSevicesPage } from './pages/bpo-sevices-page/bpo-sevices-page';
+
+/** Old Pages */
 import { HomeComponent } from './pages/home/home.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { ContactComponent } from './pages/contact/contact.component';
@@ -20,81 +34,47 @@ import { DesignAndDevelopmentComponent } from './pages/main/ourservices/design-a
 import { EmploymentPlacementComponent } from './pages/main/ourservices/employment-placement/employment-placement.component';
 import { SkillBuildingProgramsComponent } from './pages/main/ourservices/skill-building-programs/skill-building-programs.component';
 
-// export const routes: Routes = [
-//   {
-//     path: 'home',
-//     component: HomeComponent,
-//   },
-//   {
-//     path: 'about_us',
-//     component: AboutUsComponent,
-//   },
-//   {
-//     path: 'contact',
-//     component: ContactComponent,
-//   },
-//   {
-//     path: 'blog',
-//     component: BlogComponent,
-//   },
-//   {
-//     path: 'blog/december2022',
-//     component: December2022Component,
-//   },
-//   {
-//     path: 'blog/august2023',
-//     component: August2023Component,
-//   },
-//   {
-//     path: 'blog/september2023',
-//     component: September2023Component,
-//   },
-
-//   {
-//     path: 'partnership',
-//     component: PartnershipComponent,
-//   },
-//   {
-//     path: 'training',
-//     component: TrainingComponent,
-//   },
-//   {
-//     path: 'training',
-//     component: TrainingComponent,
-//   },
-//   // {
-//   //   path: 'about_us',
-//   //   component: AboutUsComponent,
-//   // },
-//   {
-//     path: 'blog',
-//     component: BlogComponent,
-//   },
-//   {
-//     path: 'partnership',
-//     component: PartnershipComponent,
-//   },
-//   { path: '**', redirectTo: 'home' } /**Add 404 page */,
-// ];
-
 export const routes: Routes = [
   /**1. STANDARD ROUTES (Main Layout) */
   {
     path: '',
     component: AppLayoutComponent,
     children: [
-      { path: 'home', component: HomeComponent },
-      { path: 'about_us', component: AboutUsComponent },
-      { path: 'contact', component: ContactComponent },
-      { path: 'blog', component: BlogComponent },
-      { path: 'blog/december2022', component: December2022Component },
+      /** New Revamp Pages */
+      { path: 'home', component: HomePage },
+      { path: 'about', component: AboutPage },
+      { path: 'contact', component: ContactPage },
+      { path: 'partner', component: PartnerPage },
+      { path: 'career', component: CareerPage },
+      { path: 'success', component: SuccessPage },
+      { path: 'tech-services', component: TechServicesPage },
+      { path: 'upskilling-services', component: UpskillingSevicesPage },
+      { path: 'our-facility', component: OurFacilityPage },
+      { path: 'programs', component: ProgramsPage },
+      { path: 'bpo-services', component: BpoSevicesPage },
+
+      /** Old Pages */
+      // { path: 'blog', component: BlogComponent },
+      // { path: 'blog/december2022', component: December2022Component },
       { path: 'partnership', component: PartnershipComponent },
       { path: 'training', component: TrainingComponent },
-      { path: 'services/ai-and-machine-learning', component: AiAndMachineLearningComponent },
+      {
+        path: 'services/ai-and-machine-learning',
+        component: AiAndMachineLearningComponent,
+      },
       { path: 'services/data-annotation', component: DataAnnotationComponent },
-      { path: 'services/design-and-development', component: DesignAndDevelopmentComponent },
-      { path: 'services/employment-placement', component: EmploymentPlacementComponent },
-      { path: 'services/skill-building-programs', component: SkillBuildingProgramsComponent },
+      {
+        path: 'services/design-and-development',
+        component: DesignAndDevelopmentComponent,
+      },
+      {
+        path: 'services/employment-placement',
+        component: EmploymentPlacementComponent,
+      },
+      {
+        path: 'services/skill-building-programs',
+        component: SkillBuildingProgramsComponent,
+      },
       { path: '', redirectTo: 'home', pathMatch: 'full' },
     ],
   },
@@ -109,11 +89,13 @@ export const routes: Routes = [
       { path: 'home', component: HomeComponent },
       { path: 'about_us', component: AboutUsComponent },
       { path: 'contact', component: ContactComponent },
-      { path: 'users', component: UsersPage
-      //   ,children: [
-      //   { path: 'profile', component: UserProfile }
-      // ]
-    },
+      {
+        path: 'users',
+        component: UsersPage,
+        //   ,children: [
+        //   { path: 'profile', component: UserProfile }
+        // ]
+      },
     ],
   },
 
