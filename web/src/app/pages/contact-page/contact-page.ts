1df4e067701a1b923d46ab794718c8badb111f20
joe052 <joekaranjasenior52@gmail.com>
2026-03-11 13:59:51 +0300

    message

--- web/src/app/pages/contact-page/contact-page.ts
+++ web/src/app/pages/contact-page/contact-page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ContactDetails } from '../../components/ContactPage/contact-details/contact-details';
+import { ContactDetails } from '../../components/contact-details/contact-details';
 import { CtaPartnerCard } from '../../components/cta-partner-card/cta-partner-card';
 
 @Component({
